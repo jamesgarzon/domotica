c8b198aea282de44bf7b57c0201fb02cea16edb2
James Garzón <james.garzon@udea.edu.co>
2016-06-15 23:22:45 -0500

    Se listan los dispositivos

--- SQL/listarDispositivosSQL.php
+++ SQL/listarDispositivosSQL.php
@@ -1,14 +1,31 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require('ConexionDB.php');
 
 // function ListarDispositivosSQL(){
 	$conexion = ConectarDB();
 	$sql =" SELECT *
 	FROM Dispositivo ";
-	$resultadoDatos = mysqli_query($conexion, $sql);
-
-	$resultadoDatosRetornar = mysqli_fetch_all($resultadoDatos,MYSQLI_ASSOC);
-	// mysqli_close($conexion);
-	// echo $resultadoDatosRetornar;
+ // $result = mysqli_query($conexion, $sql);
+ //
+ // // $resultadoDatosRetornar = mysqli_fetch_all($resultadoDatos,MYSQLI_ASSOC);
+ // while ($row = $result->fetch_assoc()) {
+ //    echo $row;
  // }
+ // mysqli_close($conexion);
+ // echo $resultadoDatosRetornar;
+ // // }
+ $result = $conexion->query($sql);
+
+// if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+			print_r($row);
+        // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+// } else {
+    // echo "0 results";
+// }
 ?>
