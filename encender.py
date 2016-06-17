97536d7263f988c21672b61348304f33546f2c3e
James Garzón <jamesgarzon92@gmail.com>
2016-06-16 23:14:48 -0500

    Se implementa configuración de pin

--- encender.py
+++ encender.py
@@ -2,10 +2,10 @@
 
 #Importamos Gpio
 import RPi.GPIO as GPIO
-
+import sys
+pin = int(sys.argv[1])
 #Le decimo que utilizamos la nomeclatura BCM
 GPIO.setmode(GPIO.BCM)
 #Aqui indicamos cual es el puerto que utilizaremos, y que es de salida
-GPIO.setup(17, GPIO.OUT)
-GPIO.output(17, True)
-
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin, True)
